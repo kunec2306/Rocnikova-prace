c96521225afe8d49d2f8b5a05408d61d85f42b48
Radim Kunc <radim.kunc@student.sps-cl.cz>
2025-03-20 19:59:57 +0100

    panel

--- public/index.js
+++ public/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd9b0f51e2ca7af3bfd5f49d31199da151f73892fbf681eeb061438eb052d9d1
-size 2156
+oid sha256:122564470583a10532d5f3af5068d39c5692c8ab35c7e9c607b45f2c1e6c08c5
+size 2728
